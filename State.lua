2952beca0f89951f3182d6ea854e434650cc86b7
mingyun.cho <chomg88@gmail.com>
2025-09-22 20:00:42 +0900

    focus range

--- State.lua
+++ State.lua
@@ -1337,6 +1337,23 @@ end
 state.timeToInterrupt = timeToInterrupt
 
 
+-- Check if interrupt ability is within range of focus target
+local function canInterruptFocus()
+    if not UnitExists( "focus" ) then return false end
+    
+    -- Use specific interrupt spell ID (47528 - 정신 얼리기)
+    local interruptSpellId = 47528
+    
+    -- Check if the interrupt spell is within range of focus
+    if LSR.IsSpellInRange( interruptSpellId, "focus" ) == 1 then
+        return true
+    end
+    
+    return false
+end
+state.canInterruptFocus = canInterruptFocus
+
+
 -- Pet stuff.
 local function summonPet( name, duration, spec )
     state.pet[ name ] = rawget( state.pet, name ) or {}
